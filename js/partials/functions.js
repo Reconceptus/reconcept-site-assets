2f9a71af2510a394c4ad6a7ef75518e0548ad121
enenkoff <enenkoff@mail.ru>
2019-01-31 06:23:42 +0200

    assets for ReConcept

---
+++ js/partials/functions.js
@@ -0,0 +1,232 @@
+var project = {},
+    scrollTop,
+    scrollVar = 0,
+    winWidth,
+    winHeight,
+    $search, $searchModal,
+    $html = document.getElementsByTagName('html'),
+    $header;
+
+/* ----------------------------------- functions ----------------------------------- */
+
+    /*
+     ============= header fixed
+    */
+
+project.headerFixed = function(){
+
+    if(scrollTop <= 80){
+        $header.removeClass('simple-header');
+        $('.sidebar').length > 0 ? $('.sidebar').removeClass('subscribe-bar') : '';
+        scrollVar = 0;
+    }
+    else if(scrollTop > scrollVar){
+        $header.addClass('simple-header');
+        $('.sidebar').length > 0 ? $('.sidebar').removeClass('subscribe-bar') : '';
+        scrollVar = scrollTop;
+    }
+    else {
+        $header.removeClass('simple-header');
+        $('.sidebar').length > 0 ? $('.sidebar').addClass('subscribe-bar') : '';
+        scrollVar = scrollTop;
+    }
+};
+
+    /*
+     ============= file input
+    */
+
+project.fileInput = function(){
+
+    var inputVox = $('.file');
+    inputVox.each(function () {
+        var _this = $(this),
+            inputEl = _this.find('input[type="file"]'),
+            infoArea = _this.find('.file-text'),
+            resetBtn = _this.find('.reset'),
+            fileNameDefault = infoArea.attr('data-default-text');
+
+        inputEl.change(function (e) {
+            var fileName = e.target.files[0].name;
+            infoArea.text(fileName);
+            _this.addClass('has-file');
+        });
+
+        resetBtn.click(function () {
+            inputEl.val('');
+            infoArea.text(fileNameDefault);
+            _this.removeClass('has-file');
+        });
+
+    });
+
+};
+
+    /*
+     ============= page overflow
+    */
+
+project.ovhEnable = function(){
+    $html[0].classList.add('ovh');
+};
+project.ovhDisable = function(){
+    $html[0].classList.remove('ovh');
+};
+
+    /*
+     ============= search modal reset
+    */
+
+project.searchReset = function(){
+    var $searchForm = $('.search-form'),
+        $searcCloneText = $searchForm.find('.clone-text .text'),
+        $searcCloneBox = $searchForm.find('.input-dublicate'),
+        $searcReset = $searchForm.find('.reset'),
+        $searchInput = $('.search-input');
+
+    function visibleReset(value){
+        var inputValue = value;
+        if(inputValue == ''){
+            $searcCloneBox.removeClass('visible');
+        }
+        else {
+            $searcCloneBox.addClass('visible');
+        }
+    }
+
+    $searchInput.keyup(function (e) {
+        var _thisVal = $(this).val();
+        $searcCloneText.text(_thisVal);
+        visibleReset(_thisVal);
+    });
+
+    $searcReset.click(function () {
+        $searcCloneText.text('');
+        visibleReset('');
+    });
+};
+
+    /*
+     ============= fixing aside
+    */
+
+project.fixingAside = function(box,boxParent,boxParentHeight,diff){
+
+    var difference = diff > 0 ? diff : 0;
+
+    var $boxOffset = boxParent.offset().top,
+        $boxHeight = box.height(),
+        maxShareScroll = $boxOffset + boxParentHeight - $boxHeight;
+
+    if(scrollTop < $boxOffset - difference){
+        box.removeClass('fixed bottom');
+    }
+    else if(scrollTop > maxShareScroll){
+        box.removeClass('fixed');
+        box.addClass('bottom');
+    }
+    else {
+        box.addClass('fixed');
+        box.removeClass('bottom');
+    }
+
+};
+
+    /*
+    ============= sticky sharing
+   */
+
+project.stickySharing = function(){
+    var $sharing = $('.sharing'),
+        $shareBox = $sharing.closest('.text-box--layout'),
+        $shareBoxHeight = $shareBox.height();
+
+    project.fixingAside($sharing,$shareBox,$shareBoxHeight);
+    $(window).scroll(function (e) {
+        project.fixingAside($sharing,$shareBox,$shareBoxHeight);
+    });
+
+};
+
+    /*
+     ============= text parsing
+    */
+
+project.textParsing = function(){
+    var $textBox = $('.text-box');
+    $textBox.each(function () {
+        var _this = $(this);
+        _this.find('iframe').wrap('<div class="video"></div>');
+
+        _this.find('img[style*="float: left"]').addClass('align-left');
+        _this.find('img[style*="float: right"]').addClass('align-right');
+        _this.find('img[style*="margin: auto"]').addClass('align-center');
+        _this.find('img[style*="width"]').addClass('has-width');
+
+        _this.find('blockquote').wrapInner('<p></p>');
+    });
+};
+
+    /*
+     ============= project link parsing
+    */
+
+project.linkParsing = function(){
+    var $link = $('.project-link'),
+        $linkText = $link.text().trim();
+
+    $link.html('<div class="link-full"></div><div class="link-parts"></div>');
+
+    for(var i=0; i<$linkText.length; i++){
+        $('.link-full, .link-parts').append('<span>'+$linkText[i]+'</span>');
+    }
+
+};
+
+    /*
+     ============= sticky sidebar
+    */
+
+project.stickySidebar = function(){
+    var $sidebar = $('.sidebar'),
+        $sidebarBox = $sidebar.closest('.page-layout'),
+        $sidebarBoxHeight = $sidebarBox.height();
+
+    project.fixingAside($sidebar,$sidebarBox,$sidebarBoxHeight,110);
+    $(window).scroll(function (e) {
+        project.fixingAside($sidebar,$sidebarBox,$sidebarBoxHeight,110);
+    });
+
+};
+
+
+/* ----------------------------------- plugins ----------------------------------- */
+
+project.carousel = function() {
+    var $carouselBox = $('[data-owl="blog"]'),
+        $carousel = $carouselBox.find('.owl-carousel');
+
+    $carousel.owlCarousel({
+        loop: true,
+        nav: true,
+        navText: ['',''],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2,
+                margin: 20
+            },
+            1201: {
+                items: 3,
+                margin: 30
+            },
+            1367: {
+                items: 3,
+                margin: 50
+            }
+        }
+    })
+};
---
+++ js/partials/scripts.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+
+    /* ----------------------------------- variables ----------------------------------- */
+
+    $header = $('#header');
+    $search = $('#showSearch');
+    $support = $('#showSupport');
+    $modal = $('.modal');
+
+    /* ------------------------------- get window values ------------------------------- */
+
+    scrollTop = $(window).scrollTop();
+    function getWindowSizes() {
+        winWidth = $(window).width();
+        winHeight = $(window).height();
+    }
+
+    /* ----------------------------------- functions ----------------------------------- */
+
+    /*
+     ============= header burger
+    */
+
+    $('#burger').click(function () {
+        $header.removeClass('simple-header');
+    });
+
+    $('#mobileBurger').click(function () {
+        $header.toggleClass('is-active');
+    });
+
+    $('.close-mobile-nav').click(function () {
+        $header.removeClass('is-active');
+    });
+
+    /*
+     ============= modals
+    */
+
+    $search.on('click',function () {
+        project.ovhEnable();
+        $header.removeClass('simple-header');
+        $('.search-modal').addClass('active');
+    });
+
+    $support.on('click',function () {
+        project.ovhEnable();
+        $header.removeClass('simple-header');
+        $('.support-modal').addClass('active');
+    });
+
+    $modal.find('.close').on('click',function () {
+        project.ovhDisable();
+        $modal.removeClass('active');
+    });
+
+    /*
+     ============= document click events
+    */
+
+    function documentClick() {
+        $(document).click(function(e){
+            // var targ = $(e.target);
+            // if(targ.parents('#searchForm').length == 0){
+            //     if(targ.parents('#search_btn').length == 0){
+            //         search.removeClass('active');
+            //         nav.removeClass('hidden');
+            //     }
+            // }
+            //
+            // if(targ.parents('.language').length == 0){
+            //     lang.removeClass('show')
+            // }
+        })
+    }
+
+
+
+
+    /* --------------------------------- document load --------------------------------- */
+
+    documentClick();
+    project.headerFixed();
+    getWindowSizes();
+    if($('.search-form').length > 0){
+        project.searchReset();
+    }
+    if($('.sharing').length > 0){
+        project.stickySharing();
+    }
+    if($('.sidebar').length > 0){
+        project.stickySidebar();
+    }
+    if($('.file').length > 0){
+        project.fileInput();
+    }
+    if($('.text-box').length > 0){
+        project.textParsing();
+    }
+    if($('.project-link').length > 0){
+        project.linkParsing();
+    }
+
+
+    /* --------------------------------- document resize --------------------------------- */
+
+    $(window).resize(function () {
+        getWindowSizes();
+    });
+
+    /* --------------------------------- document scroll --------------------------------- */
+
+    $(window).scroll(function (e) {
+        scrollTop = $(window).scrollTop();
+        project.headerFixed();
+    });
+
+});
